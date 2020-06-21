4396e6fa8f4b4f42780fd21865f7430dda16b013
kuromu0104 <nccjb457@yahoo.co.jp>
2020-06-21 14:30:02 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   root 'groups#index'
   resources :users, only: [:edit, :update]
   resources :groups, only: [:index, :new, :create, :edit, :update]
-  resources :messages, only: [:index, :create]
+    resources :messages, only: [:index, :create]
+    
     namespace :api do
       resources :messages, only: :index, defaults: { format: 'json' }
     end

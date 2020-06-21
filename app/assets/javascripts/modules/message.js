7104708b565ae8af77bb5c236a2d47964a566d28
kuromu0104 <nccjb457@yahoo.co.jp>
2020-06-21 15:06:25 +0900

    Update message.js

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -60,7 +60,8 @@ $(function(){
       $('forms')[0].reset();
       $('messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
       $('.form-submit').prop("disabled", false);
-    })
+    }
+  })
     .fail(function() {
       alert("メッセージ送信に失敗しました");
       $('.form-submit').prop("disabled", false);

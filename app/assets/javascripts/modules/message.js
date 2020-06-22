ac6561f439e577bd82e5014332627724c93e7e19
kuromu0104 <nccjb457@yahoo.co.jp>
2020-06-22 17:26:59 +0900

    commit

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -4,7 +4,7 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       let html =
-        `<div class="messages" data-message-id=${message.id}>
+        `<div class="message" data-message-id=${message.id}>
           <div class="message">
             <div class="message__list">
             <div class="message__list__name">
@@ -24,13 +24,15 @@ $(function(){
       return html;
     } else {
       let html =
-      `<div class="messages" data-message-id=${message.id}>
+      `<div class="message" data-message-id=${message.id}>
         <div class="message">
-          <div class="message__list__name">
-            ${message.user_name}
-          </div>
-          <div class="message__list__date">
-            ${message.created_at}
+          <div class="message__list">
+            <div class="message__list__name">
+              ${message.user_name}
+            </div>
+            <div class="message__list__date">
+              ${message.created_at}
+            </div>
           </div>
         </div>
         <div class="message__box">
@@ -45,32 +47,34 @@ $(function(){
 
   $('.forms').on('submit', function(e){
     e.preventDefault();
-    var reloadMessages = function() {
-    let formData = new FormData(this);
-    let url = $(this).attr('action');
-    var last_message_id = $('.message:last').data("message-id");
-    $.ajax({
-      url: "api/messages",
-      type: "POST",
-      data: {id: last_message_id},
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(data) {
-      if (messages.length !== 0) {
-      let html = buildHTML(data);
-      $('.messages').append(html);
-      $('forms')[0].reset();
-      $('messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-      $('.form-submit').prop("disabled", false);
-    }
+    // var reloadMessages = function() {
+      let formData = new FormData(this);
+      let url = $(this).attr('action');
+      var last_message_id = $('.message:last').data("message-id");
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+      .done(function(messages) {
+        if (messages.length !== 0) {
+        let html = buildHTML(messages);
+        $('.messages').append(html);
+        $('.forms')[0].reset();
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+        $('.form-submit').prop("disabled", false);
+        }
+      })
+      .fail(function() {
+        alert("メッセージ送信に失敗しました");
+        $('.form-submit').prop("disabled", false);
+      });
   })
-    .fail(function() {
-      alert("メッセージ送信に失敗しました");
-      $('.form-submit').prop("disabled", false);
-    });
-  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-    setInterval(reloadMessages, 7000);
-  }
+    // if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    //   setInterval(reloadMessages, 7000);
+    // }
+
 })
